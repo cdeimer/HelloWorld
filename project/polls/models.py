722c4a88f65b9af4f5b38596403ea3404f7eb831
cdeimer <cullendeimer@gmail.com>
2022-10-30 20:11:24 -0400

    finished the beginner tutorial

--- project/polls/models.py
+++ project/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.utils import timezone
 from django.db import models
 
@@ -11,9 +12,14 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
-        return now >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
